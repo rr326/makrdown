888c50757858eeda231ec366885488bebd9033bf
Ross Rosen <rrosen326@gmail.com>
2014-05-17 06:00:58 -0700

    working

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 /*global module:false*/
 module.exports = function(grunt) {
+  require('load-grunt-tasks')(grunt);
+
 
   // Project configuration.
   grunt.initConfig({
@@ -31,6 +33,18 @@ module.exports = function(grunt) {
     nodeunit: {
       files: ['test/**/*_test.js']
     },
+        markdown: {
+            all: {
+                files: [
+                    {
+                        expand: true,
+                        src: ['*.md'],
+                        dest: '.tmp/mdcompiled',
+                        ext: '.html'
+                    }
+                ]
+            }
+        },    
     watch: {
       gruntfile: {
         files: '<%= jshint.gruntfile.src %>',
@@ -39,16 +53,58 @@ module.exports = function(grunt) {
       lib_test: {
         files: '<%= jshint.lib_test.src %>',
         tasks: ['jshint:lib_test', 'nodeunit']
+      },
+
+      md: {
+        files: ['*.md'],
+        tasks: ['markdown'],
+        options: {
+          spawn: false,
+          livereload: 35731
+        }
       }
-    }
-  });
+    },
+
+        // The actual grunt server settings
+        connect: {
+          options: {
+            port: 9000,
+            hostname: 'localhost',
+            livereload: 35729
+          },
+
+          md: {
+            options: {
+              open: true,
+              base: ['.tmp/mdcompiled'],
+              port: 9002,
+              livereload: 35731
+            }
+          }
+        },
+
+
+      });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task.
-  grunt.registerTask('default', ['jshint', 'nodeunit']);
+  grunt.registerTask('serve', function (target) {
+
+
+    grunt.task.run([
+                'markdown', // Initialize
+                'connect:md',
+                'watch:md'
+                ]);
+
+  });
 
 };
+
+
+
+
+
